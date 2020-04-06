bc328124f03c540c92254d28c418e37a84aa2731
Jakub Spałek <spalek9822@gmail.com>
2020-04-06 16:37:29 +0200

    Lab6 ex.2

--- jakub_spalek_lab6_32bit/jakub_spalek_lab6_32bit/jakub_spalek_lab6_32bit.cpp
+++ jakub_spalek_lab6_32bit/jakub_spalek_lab6_32bit/jakub_spalek_lab6_32bit.cpp
@@ -4,6 +4,7 @@ void SumMatrices(int** matrixA, int** matrixB, int** result, int rows, int cols)
 void MultiplyMatrices(int** matrixA, int** matrixB, int** result, int rowsA, int colsA, int colsB);
 int MinMatrix3D(int*** matrix, int rows, int cols, int depth);
 void PrintMatrix2D(int** matrix, int rows, int cols);
+void PrintMatrix3D(int*** matrix, int rows, int cols, int depth);
 
 int main()
 {
@@ -139,9 +140,108 @@ int main()
 
     delete[] matrixA, matrixB;
 
+    //+++++++++++++++++++++++++++++++++++++++++++++++
+
+    std::cout << "---=== MIN Z MACIERZY TROJWYMIAROWEJ ===---\n";
+    int rows3D, cols3D, depth;
+
+    std::cout << "Podaj X macierzy 3D: ";
+    std::cin >> rows3D;
+    std::cout << "Podaj Y macierzy 3D: ";
+    std::cin >> cols3D;
+    std::cout << "Podaj Z macierzy 3D: ";
+    std::cin >> depth;
+
+    int*** matrix3D = new int** [rows3D];
+
+    for (int i = 0; i < rows3D; i++)
+    {
+        matrix3D[i] = new int* [cols3D];
+
+        for (int j = 0; j < cols3D; j++)
+        {
+            matrix3D[i][j] = new int[depth];
+        }
+    }
+    
+    for (int x = 0; x < rows3D; x++)
+    {
+        for (int y = 0; y < cols3D; y++)
+        {
+            for (int z = 0; z < depth; z++)
+            {
+                printf("[%d][%d][%d]: ", x, y, z);
+                std::cin >> matrix3D[x][y][z];
+            }
+        }
+    }
+
+    PrintMatrix3D(matrix3D, rows3D, cols3D, depth);
+    int min = MinMatrix3D(matrix3D, rows3D, cols3D, depth);
+
+    std::cout << "Minimalna wartosc w tej macierzy wynosi: " << min << std::endl;
+
+    for (int i = 0; i < rows3D; i++)
+    {
+        for (int j = 0; j < cols3D; j++)
+        {
+            delete[] matrix3D[i][j];
+        }
+        delete[] matrix3D[i];
+    }
+    delete[] matrix3D;
+
     system("pause");
 }
 
+int MinMatrix3D(int*** matrix, int rows, int cols, int depth)
+{
+    int min = 0;
+
+    __asm {
+        push esi
+        push ebx
+        mov esi, matrix                 // adres macierzy
+        mov ecx, rows                   // przygotowanie iteratora i dla for1
+        
+        mov edx, [esi + 4 * ecx - 4]    // matrix[rows-1]
+        mov edx, [edx + 4 * ecx - 4]    // matrix[cols-1]
+        mov eax, [edx + 4 * ecx - 4]    // eax = matrix[depth-1] - zapisanie ostatniej wartoœci jako minimalnej
+        mov min, eax
+
+        for1:
+            mov esi, matrix
+            mov edx, [esi + 4 * ecx - 4]    // matrix[i]
+
+            push ecx                // i = [esp]
+            mov ecx, cols           // przygotowanie iteratora j dla for2
+
+            for2:
+                mov esi, edx
+                mov esi, [esi + 4 * ecx - 4]    // matrix[i][j]
+                
+                push ecx            // i = [esp-4] j = [esp]
+                mov ecx, depth      // przygotowanie iteratora k dla for3
+                
+                for3:
+                    mov ebx , [esi + 4 * ecx - 4]
+                    
+                    cmp eax, ebx                    // porównaj min z matrix[i][j][k]
+                    cmovg eax, ebx                  // je¿eli matrix[i][j][k] < min to min = matrix[i][j][k]
+                    loop for3                       // dopoki k > 0 skacz do for3
+                pop ecx
+                loop for2           // dopoki j > 0 skacz do for2
+            pop ecx
+            loop for1               // dopoki i > 0 skacz do for1
+               
+        mov min, eax                // zapisanie wyniku
+        
+        pop ebx
+        pop esi
+    }
+
+    return min;
+}
 void SumMatrices(int** matrixA, int** matrixB, int** result, int rows, int cols)
 {
     __asm {
@@ -186,7 +286,6 @@ void SumMatrices(int** matrixA, int** matrixB, int** result, int rows, int cols)
     }
     
 }
-
 void MultiplyMatrices(int** matrixA, int** matrixB, int** result, int rowsA, int colsA, int colsB)
 {
     __asm {
@@ -242,6 +341,20 @@ void PrintMatrix2D(int** matrix, int rows, int cols)
         for (int y = 0; y < cols; y++)
         {
             std::cout << matrix[x][y] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+void PrintMatrix3D(int*** matrix, int rows, int cols, int depth)
+{
+    for (int x = 0; x < rows; x++)
+    {
+        for (int y = 0; y < cols; y++)
+        {
+            for (int z = 0; z < depth; z++)
+            {
+                std::cout << matrix[x][y][z] << " ";
+            }
         }
         std::cout << std::endl;
     }
